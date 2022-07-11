fa2e674752184df040059caed4698154a27e2cb1
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-07-11 16:55:39 -0700

    connect chatbox to firebase db

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import "./styles/_global.scss";
 import styles from "./index.module.scss";
 import { SignIn, SignOut } from "./components/SignInButtons";
 import * as firebase from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  orderBy,
+  limit,
+  query,
+  FirestoreDataConverter,
+  WithFieldValue,
+  DocumentData,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
@@ -21,24 +32,64 @@ const app = firebase.initializeApp({
 });
 
 const auth = getAuth();
-const firestore = getFirestore();
+const db = getFirestore(app);
 
-const SignIn: React.FC = () => {
-  const signInWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    await signInWithPopup(auth, provider);
-  };
+type Message = {
+  id: string;
+  message: string;
+  created_at: string;
+};
+
+const messageConverter: FirestoreDataConverter<Message> = {
+  toFirestore(message: WithFieldValue<Message>): DocumentData {
+    return {
+      id: message.id,
+      message: message.message,
+      created_at: message.created_at,
+    };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Message {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      message: data.message,
+      created_at: data.created_at,
+    };
+  },
+};
+
+const Chatbox: React.FC = () => {
+  const messageRef = collection(db, "messages").withConverter(messageConverter);
+  const q = query(messageRef, orderBy("created_at"), limit(25));
+  const [data, loading, error] = useCollectionData(q);
 
   return (
-    <>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
-    </>
+    <div className={styles.Chatbox}>
+      <header className="App-header">Hello World :fire:</header>
+      {loading ? (
+        <p>loading...</p>
+      ) : error ? (
+        <p>error loading data!</p>
+      ) : (
+        data &&
+        data.map((message) => (
+          <ChatMessage message={message} key={message.id} />
+        ))
+      )}
+    </div>
   );
 };
 
-const SignOut: React.FC = () => {
+const ChatMessage: React.FC<{ message: Message }> = ({ message }) => {
   return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+    <>
+      <div>
+        <p>{message.message}</p>
+      </div>
+    </>
   );
 };
 
